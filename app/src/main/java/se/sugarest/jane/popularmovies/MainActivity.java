b09c6eacfd0c964188a2a8bfc392721df11b0cdc
JinyanLiu <jane.jinyan.liu@gmail.com>
2017-02-26 14:37:03 +0100

    Set up RecyclerView in MainActivity.java file.

--- app/src/main/java/se/sugarest/jane/popularmovies/MainActivity.java
+++ app/src/main/java/se/sugarest/jane/popularmovies/MainActivity.java
@@ -1,13 +1,45 @@
 package se.sugarest.jane.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private RecyclerView mRecyclerView;
+
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * Using findViewById, get a reference to the RecyclerView from xml.
+         */
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_movieposters);
+
+        /**
+         * this: Current context, will be used to access resources.
+         * 2: The number of columns or rows in the grid
+         * GridLayoutManager.VERTICAL: Layout orientation.
+         * false: When set to true, layouts from end to start.
+         */
+        GridLayoutManager layoutManager
+                = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        /**
+         * Use this setting to improve performance that changes in content do not change the child
+         * layout size in the RecyclerView.
+         */
+        mRecyclerView.setHasFixedSize(true);
+
     }
 }
